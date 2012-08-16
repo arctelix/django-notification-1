a3dd0643812cee5d0f613376de16306310c980eb
Wladston Viana Ferreira Filho <wladston@wladston.net>
2012-08-15 21:21:52 -0300

    Small bugfixes

--- notification/backends/website.py
+++ notification/backends/website.py
@@ -1,8 +1,8 @@
 # Django
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.contenttypes.models import ContentType 
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
 # PickleField
@@ -12,13 +12,14 @@ from picklefield.fields import PickledObjectField
 from notification import backends
 from notification.models import NoticeType
 
+
 class NoticeManager(models.Manager):
 
     def notices_for(self, user, archived=False, unseen=None):
         """
         returns Notice objects for the given user.
-        archived : { False: only messages not archived, True: all messages } 
-        unseen : {None: all notices, True: only unseen, False: only see notices}
+        archived : { False: only messages not archived, True: all messages }
+        unseen : {None: all notices, True: only unseen, False: only seen}
         """
         qs = self.filter(recipient=user)
         if not archived:
@@ -33,7 +34,8 @@ class NoticeManager(models.Manager):
         This function is tipically called when the receiver views the sender,
         so the notification about the sender isn't "fresh" anymore.
         '''
-        if receiver.is_anonymous(): return
+        if receiver.is_anonymous():
+            return
         ctype = ContentType.objects.get_for_model(sender)
         for n in self.filter(content_type=ctype, object_id=sender.id,
                              recipient=receiver):
@@ -53,7 +55,7 @@ class Notice(models.Model):
     '''
     recipient = models.ForeignKey(User, verbose_name=_("recipient"))
     notice_type = models.ForeignKey(NoticeType, verbose_name=_("notice type"))
-    added = models.DateTimeField(_("added"), auto_now = True)
+    added = models.DateTimeField(_("added"), auto_now=True)
     unseen = models.BooleanField(_("unseen"), default=True)
     archived = models.BooleanField(_("archived"), default=False)
 
@@ -62,7 +64,7 @@ class Notice(models.Model):
     # Polymorphic relation to allow any object to be the sender
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
-    sender = generic.GenericForeignKey("content_type", "object_id") 
+    sender = generic.GenericForeignKey("content_type", "object_id")
 
     objects = NoticeManager()
 
@@ -77,7 +79,7 @@ class Notice(models.Model):
         """
         Render the notification with the given template.
         """
-        self.data.update({'self':self})
+        self.data.update({'self': self})
         return backends.format_notification(template,
                                             self.notice_type.label,
                                             self.data)
@@ -96,7 +98,7 @@ class Notice(models.Model):
         return unseen
 
     class Meta:
-        app_label = 'notification' # needed for syncdb
+        app_label = 'notification'  # needed for syncdb
         ordering = ["-added"]
         verbose_name = _("notice")
         verbose_name_plural = _("notices")
@@ -104,7 +106,7 @@ class Notice(models.Model):
 
 class WebsiteBackend(backends.BaseBackend):
     """
-    Stores the notification on the website, they will be shown when the user 
+    Stores the notification on the website, they will be shown when the user
     visit the proper notification page.
     """
     spam_sensitivity = 1
@@ -113,7 +115,7 @@ class WebsiteBackend(backends.BaseBackend):
         """
         Just saves the notification to the database, it gets displayed
         """
-        Notice.objects.create(recipient = recipient,
-                              sender = sender,
-                              data = extra_context,
-                              notice_type = notice_type)
+        Notice.objects.create(recipient=recipient,
+                              sender=sender,
+                              data=extra_context,
+                              notice_type=notice_type)
