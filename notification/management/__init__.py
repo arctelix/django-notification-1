2443ca2c5796885019e7920935b59d415b3f27c3
arctelix <arctelix@gmail.com>
2013-06-15 11:35:42 -0400

    update_notice_types mc & NOTICE_TYPES setting
    -Standarizes notice_type definition and creation.
     define notice types in NOTICE_TYPES setting as list of tupples
     and django-notification takes care of the rest.
    -south migrations to do not send the post_syncdb signal so this command
     allows you to manually update your notice types table.
    
    asdf

---
+++ notification/management/__init__.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db.models import signals
+from django.utils.translation import ugettext_noop as _
+
+notice_types = getattr(settings, 'NOTICE_TYPES', None)
+def create_notice_types():
+    for type in notice_types:
+        notification.create_notice_type(type[0],_(type[1]), _(type[2]))
+    print 'notice types are now up to date'
+
+if "notification" in settings.INSTALLED_APPS:
+    from notification import models as notification
+    def run_create_notice_types(app, created_models, verbosity, **kwargs):
+        if app == notification:
+            print 'creating notice types'
+            create_notice_types()
+    signals.post_syncdb.connect(run_create_notice_types, sender=notification)
+else:
+    print "Skipping creation of NoticeTypes as notification app not found"
+    
