4ee5c6f0e4c55fc33eda6c0f2ee32f7847e5eec9
arctelix <arctelix@gmail.com>
2014-10-29 15:55:42 -0400

    notification_settings template tag update for anon user

--- notification/templatetags/notification_tags.py
+++ notification/templatetags/notification_tags.py
@@ -1,6 +1,7 @@
 from django.template import Library
 from django.conf import settings
-from notification.models import NoticeType, get_notification_setting, NOTICE_MEDIA
+from django.contrib.auth.models import User
+from notification.models import NoticeType, get_notification_setting, NOTICE_MEDIA, NoticeSetting
 
 register = Library()
 
@@ -11,11 +12,21 @@ def notification_settings(context):
     '''
     user = context['request'].user
     user_settings = []
+    # Get list of user's settings
     if user.is_authenticated():
         notice_types = NoticeType.objects.all()
         for notice in notice_types:
             for media in NOTICE_MEDIA:
                 setting = get_notification_setting(user, notice, media[0])
                 #setting.send = 'yes' if setting.send else ''
+                user_settings.append(setting)
+
+    # Get list of available settings
+    else:
+        notice_types = NoticeType.objects.all()
+        for notice in notice_types:
+            for media in NOTICE_MEDIA:
+                setting = NoticeSetting(user=User(), notice_type=notice, medium=media[0])
+                setting.send = True
                 user_settings.append(setting)
     return user_settings
